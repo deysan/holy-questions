6974121634e4d97b50f7de3dcfb58a480f454b05
deysan <ideysan@gmail.com>
2022-01-09 01:45:58 +0200

    Fix style

--- src/app/components/Emoji.jsx
+++ src/app/components/Emoji.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 
+const style = {
+  fontSize: '20px'
+};
+
 const Emoji = ({ label, icon, nospace }) => (
   <span
     className="emoji"
     role="img"
     aria-label={label ? label : ''}
     aria-hidden={label ? 'false' : 'true'}
+    style={style}
   >
     {`${icon}`}
     {nospace ? `` : ` `}
