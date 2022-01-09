e60eb25e91bd89b1dfc42606b3dfcdd119de18d2
deysan <ideysan@gmail.com>
2022-01-09 19:09:45 +0200

    Add progress status

--- src/app/components/Status.jsx
+++ src/app/components/Status.jsx
@@ -5,9 +5,13 @@ import { prevQuestion } from '../store/quizSlice';
 const Status = ({ currentQuestion, allQuestions }) => {
   const dispatch = useDispatch();
 
+  const progressStatus = {
+    '--progress-status': `${((currentQuestion + 1) * 100) / allQuestions}%`
+  };
+
   return (
     <div className="status-bar">
-      <span className="status-questions">
+      <span className="status-questions" style={progressStatus}>
         {currentQuestion + 1}/{allQuestions}
       </span>
       {currentQuestion > 0 && (
